8e0c566826ef91d6bcc005d325de7d6c5e7e97a3
NinaMiroshnychenko <nina.m@medichi.com.ua>
2020-02-04 13:50:44 +0200

    Updates

---
+++ precache-manifest.68168762385098c8fccca6eb33af1298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db71d61766209775eb89f83e5571bc39",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6a8d1c0cd259a7dac468",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.8c93b25c.chunk.css"
+  },
+  {
+    "revision": "af9304209e6bf43450e7",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d10a58d3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d10a58d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8d1c0cd259a7dac468",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.03771944.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
